a2723f81236ab6808b3ef76d19f05aeff080a13c
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-11-12 19:15:21 +0100

    Fix namespase in web

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::middleware('auth')->namespace('Admin')->name('admin')->prefix('admin')->group(function(){
+Route::middleware('auth')->namespace('admin')->name('admin')->prefix('admin')->group(function(){
     Route::get('/', 'HomeController@index')->name('home');
 });
 
