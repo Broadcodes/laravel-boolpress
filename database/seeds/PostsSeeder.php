7ac34cffd6e6e2436f16ae892614cb20ddaadbb1
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-11-14 23:16:49 +0100

    Popolazione seeder con faker

--- database/seeds/PostsSeeder.php
+++ database/seeds/PostsSeeder.php
@@ -1,16 +1,32 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Posts;
+use Illuminate\Support\Str;
 
 class PostsSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 30; $i++) {
+            $post = new Posts();
+            $post->title = $faker->sentence(3);
+            $post->description = $faker->paragraph();
+
+            $slug = Str::slug($post->title);
+            $slug_base = $slug;
+            $counter = 1;
+            $existingPost = Posts::where('slug', $slug)->first();
+            while($existingPost){
+                $slug = $slug_base . '_' . $counter;
+                $counter++;
+                $existingPost = Posts::where('slug', $slug)->first();
+            }
+
+            $post->slug = $slug;
+
+            $post->save();
+        }
     }
 }
