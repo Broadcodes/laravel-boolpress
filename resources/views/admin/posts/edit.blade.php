af8ed549666e10e93cddf8feb98edb791e431beb
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-11-15 09:53:07 +0100

    Struttura per edit

---
+++ resources/views/admin/posts/edit.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    <div class="row">
+        <div class="col-6">
+            <form action="{{ route('admin.posts.update', $posts->id) }}" method="POST">
+                @csrf
+                @method('PUT')
+                <div class="m-2">
+                    <label for="title">Titolo</label>
+                    <input type="text" name="title" id="title">
+                </div>
+                <div class="m-2">
+                    <label for="description">Descrizione</label>
+                    <input type="text" name="description" id="description">
+                </div>
+                <div class="m-2">
+                    <input type="submit" value="Modifica">
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
