5d85dd49c0d70965eec43b5aae0646c889d79858
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-11-15 19:24:45 +0100

    Implementazione funzione per mettere in relazione le due tabelle category con post

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = ['title', 'description', 'slug'];
+
+    public function categories(){
+        return $this->belongsTo('App\Category');
+    }
 }
