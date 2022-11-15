34b4807545c9e797285a0a50c62762a39ec025b0
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-11-15 18:47:30 +0100

    Popolamento dati in categories

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -1,16 +1,20 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Category;
+use Illuminate\Support\Str;
 
 class CategorySeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        //
+        $categories = ['Intervista', 'Recensioni', 'Sport', 'Natura', 'Gossip', 'Comici', 'Spettacolo', 'News', 'Makeup', 'Altro'];
+
+        foreach ($categories as $categoryItem) {
+            $category = new Category();
+            $category->name = $categoryItem;
+            $category->slug = Str::slug($category->name);
+            $category->save();
+        }
     }
 }
