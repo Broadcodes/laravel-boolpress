624997e0b57ddd71cd83e3ac03e23306e3505946
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-11-15 20:42:30 +0100

    Condizione per visualizzare la categoria

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -5,7 +5,13 @@
         <div class="col-12 p-1 border border-dark">
 
             <h2>{{ $post->title }}</h2>
-            <p>{{ $post->category }}</p>
+
+            @if ($post->category)
+                <p>{{ $post->category->name }}</p>
+            @else
+                <p>Nessuna categoria</p>
+            @endif
+
             <p>{{ $post->description }}</p>
             <div class="d-flex">
                 <a href="{{ route('admin.posts.edit', $post->id) }}">Edit</a>
