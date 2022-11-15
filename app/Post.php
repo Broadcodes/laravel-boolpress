55dec8f863a742d34ae5551e836239b0855fb097
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-11-15 19:52:59 +0100

    Modifica fillable

--- app/Post.php
+++ app/Post.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    protected $fillable = ['title', 'description', 'slug'];
+    protected $fillable = ['title', 'description', 'slug', 'category_id'];
 
     public function categories(){
         return $this->belongsTo('App\Category');
