9c6ee689e86b8bcb1250280c606d95e020f07191
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-11-16 18:14:03 +0100

    Implementazione dati nel seeder

--- database/seeds/TagSeeder.php
+++ database/seeds/TagSeeder.php
@@ -1,16 +1,20 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Tag;
+use Illuminate\Support\Str;
 
 class TagSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        //
+        $tags = ['Pop', 'Rock', 'Metal', 'Country', 'Auto', 'Moto', 'Elettrico', 'Arte', 'Moda', 'Teatro', 'Viaggio'];
+
+        foreach ($tags as $tagModel) {
+            $tag = new Tag();
+            $tag->name = $tagModel;
+            $tag->slug = Str::slug($tag->name);
+            $tag->save();
+        }
     }
 }
