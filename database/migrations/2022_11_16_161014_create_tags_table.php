ef65ce37a2275e51bd4f7823b4e5eacc7357cb70
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-11-16 17:36:31 +0100

    Implementazione migration tags

--- database/migrations/2022_11_16_161014_create_tags_table.php
+++ database/migrations/2022_11_16_161014_create_tags_table.php
@@ -6,24 +6,16 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateTagsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 30);
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('tags');
